fe64f305089be92e88dbfb21b71e0d1ed269ba0d
robodair <robodair@users.noreply.github.com>
2016-10-03 22:48:46 +1100

    Rewrote history and removed hard coded credentials

---
+++ src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AngularFireModule, AngularFire } from 'angularfire2';
+import { FirebaseModule } from '../firebase'; // The firebase configuration
+
+// Observable.map support
+import 'rxjs/add/operator/map';
+
+// Angular2 Materialize support
+import 'angular2-materialize';
+import { MaterializeModule } from 'angular2-materialize';
+
+import { SudoMembershipsRoutingModule }  from './app-routing.module';
+
+import { AuthGuard } from './auth-guard';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MembersComponent } from './members/members.component';
+import { EditMemberComponent } from './members/edit-member/edit-member.component';
+import { PaymentsComponent } from './payments/payments.component';
+import { ExecutiveComponent } from './executive/executive.component';
+import { EditPaymentComponent } from './payments/edit-payment/edit-payment.component';
+import { UsersComponent } from './users/users.component';
+import { UserProfileComponent, UserProfileResolver } from './users/user-profile/user-profile.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        MembersComponent,
+        EditMemberComponent,
+        PaymentsComponent,
+        ExecutiveComponent,
+        EditPaymentComponent,
+        UsersComponent,
+        UserProfileComponent,
+    ],
+    imports: [
+        MaterializeModule,
+        FirebaseModule,
+        SudoMembershipsRoutingModule,
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+    ],
+    providers: [
+        AuthGuard,
+        UserProfileResolver
+        ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
