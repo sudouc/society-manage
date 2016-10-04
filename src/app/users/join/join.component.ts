889ccb1d872ba7e7a8b859b26c7b8cf4c9265b38
robodair <robodair@users.noreply.github.com>
2016-10-04 14:19:48 +1100

    Lookup member invites and check if user may accept

--- src/app/users/join/join.component.ts
+++ src/app/users/join/join.component.ts
@@ -9,16 +9,51 @@ import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
 })
 export class JoinComponent implements OnInit {
 
+    private token: String;
+
+    invitation;
+    loadedInvite = false;
+    accountInfo: FirebaseObjectObservable<any>;
+
     constructor(private af: AngularFire, private route: ActivatedRoute) { }
 
     ngOnInit() {
         // get the token from the url
         this.route.params.forEach((params: Params) => {
-            let token = params['token'];
-            if (token) {
-                console.log('[Join] activated with token: ' + token);
+            this.token = params['token'];
+            if (this.token) {
+                console.log('[Join] looking up invitation for token: ' + this.token);
+
+                // Look up the corresponding invite to the token
+                this.af.database.object('invitations/' + this.token)
+                    .subscribe(
+                        data => {                       // If there is an invitation, show it
+                            this.invitation = data;
+                            this.loadedInvite = true;
+                        },
+                        err => {                        // If there is not an invitation, say so
+                            console.log(err);
+                            this.loadedInvite = true;
+                        }
+                    );
+            }
+        });
+
+        // Check whether the logged in user is eligible for a membership connect
+        this.route.data.forEach((data: { auth: any }) => {
+            if (data.auth) {
+                let auth = data.auth.auth;
+                this.accountInfo = this.af.database.object('/users/' + auth.uid);   // info including membership & token
+
+                if (this.token) {
+                    this.af.database.object('/users/' + auth.uid + '/join_token')       // Write the temp token to the user info
+                        .set(this.token);
+                }
             }
         });
     }
 
+    acceptInvite() {
+        // Make accept invite writes in the database
+    }
 }
